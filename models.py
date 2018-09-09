0250886bdec61787b52afbfe9c8f2c6342a52746
mlfreer <1991F@inbox.ru>
2018-09-09 14:19:39 +0200

    2P-PD draft 1
    drafting the design for RMS: T1 (2person prisoners dilema)
    
    DONE:
    game interface
    instructions
    payoffs per round
    rematching
    
    TO BE DONE:
    fix the protocol
    final payoff computations
    final payoff screen
    setting the rooms
    adding other treatments

--- models.py
+++ models.py
@@ -1,29 +1,71 @@
 from otree.api import (
-    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
-    Currency as c, currency_range
+	models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+	Currency as c, currency_range
 )
 
+# to import the randomizer
+import random
 
-author = 'Your name here'
+
+
+author = 'Mikhail Freer'
 
 doc = """
-Your app description
+Part of the experimental design for the Revealed Markov Strategies project.
+Experiment for 2 person infinitely repeated prisoners dilemma.
 """
 
 
 class Constants(BaseConstants):
-    name_in_url = 'RMS'
-    players_per_group = None
-    num_rounds = 1
+	name_in_url = '2PPD'
+	players_per_group = 2
+	num_rounds = 10 # the total is 160 periods with random rematching at \delta = .8 (!!!) 
+	# set to 10 in the test mode, move to 160 in the real mode!
+
+	delta = .8 # continuation probability (in the same matching)
+
+	# defining game structure
+	CC_payoff = 40 # payoff for each player if both cooperate
+	DD_payoff = 25 # payoff to the palyer if both defect
+	CD_payoff = 10 # payoff to (C,D) profile (to the player who cooperates) 
+	DC_payoff = 60 # payoff to (D,C) profile (to the player who defects)
 
 
 class Subsession(BaseSubsession):
-    pass
-
+	match_number = models.IntegerField(initial=1) # counting the mathes (to compute the payoffs in the end)
+	rematched = models.BooleanField(initial=0) # whether you are rematched
+	random_number = models.FloatField() # to track the random number
 
 class Group(BaseGroup):
-    pass
+	pass
 
 
 class Player(BasePlayer):
-    pass
+	action = models.BooleanField() # action taken in the current round
+	payoff_in_round = models.IntegerField(initial=0) #payoff received in the current round
+
+
+	#getting current partner
+	def other_player(self):
+		return self.get_others_in_group()[0]
+
+
+
+	# setting the payoff earned in the current round
+	def set_payoff_in_round(self):
+
+		payoff_matrix = {
+			1:
+				{
+					1: Constants.CC_payoff,
+					0: Constants.CD_payoff
+				},
+			0:
+				{
+					1: Constants.DC_payoff,
+					0: Constants.DD_payoff
+				}
+		}
+
+		self.payoff_in_round = payoff_matrix[self.action][self.other_player().action]
+
--- pages.py
+++ pages.py
@@ -2,23 +2,86 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+import random 
 
-class MyPage(Page):
-    pass
+
+
+class Welcome(Page):
+	def is_displayed(self):
+		return self.round_number == 1
+	def vars_for_template(self):
+		return { 
+		'prob' : Constants.delta*100
+		}
+
+#class necessary to determine whether you are rematched
+class ShuffleWaitPage(WaitPage):
+	wait_for_all_groups = True
+
+	def after_all_players_arrive(self):
+		if self.subsession.round_number>1:
+			self.subsession.match_number = self.subsession.in_round(self.subsession.round_number-1).match_number
+
+	#checking whether to continue with the same person or not
+			r = random.uniform(0,1)
+			self.subsession.random_number = r
+	# shuffling upon the rnadom number exceeding the continuation probability
+			if r> Constants.delta:
+				self.subsession.group_randomly() # shuffle groups
+				self.subsession.match_number = self.subsession.match_number+1 # increase the match number in the next period
+				self.subsession.rematched = True # indicate that players will be rematched
+
+
+
+
+
+
+class Decision(Page):
+	form_model = 'player'
+	form_fields = ['action']
+
+	def vars_for_template(self):
+		me = self.player
+		return {
+			'rematched': me.subsession.rematched,
+			'continue': me.subsession.round_number>1
+		}
 
 
 class ResultsWaitPage(WaitPage):
 
-    def after_all_players_arrive(self):
-        pass
+	def after_all_players_arrive(self):
+		for p in self.group.get_players():
+			p.set_payoff_in_round()
+		# defining the match number for the next round !!! 
 
 
 class Results(Page):
-    pass
+	def vars_for_template(self):
+		me = self.player
+		opponent = me.other_player()
+		return {
+			'my_decision': me.action,
+			'opponent_decision': opponent.action,
+			'same_choice': me.action == opponent.action,
+		}
+
+class FinalResultsWaitPage(WaitPage):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+
+class FinalResults(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
 
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+	Welcome,
+	ShuffleWaitPage,
+	Decision,
+	ResultsWaitPage,
+	Results,
+	FinalResultsWaitPage,
+	FinalResults
+
 ]
