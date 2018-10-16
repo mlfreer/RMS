ebb099d0af0af3a9c14d2065118a57410267c463
管狐 <kokudagitsune@gmail.com>
2018-10-16 09:58:34 -0400

    160 rounds
    160 rounds

--- RMS/models.py
+++ RMS/models.py
@@ -24,7 +24,7 @@ Experiment for 2 person infinitely repeated prisoners dilemma.
 class Constants(BaseConstants):
 	name_in_url = '2PPD'
 	players_per_group = 2
-	num_rounds = 3 # the total is 160 periods with random rematching at \delta = .8 (!!!)
+	num_rounds = 160 # the total is 160 periods with random rematching at \delta = .8 (!!!)
 	# set to 10 in the test mode, move to 160 in the real mode!
 
 	delta = .8 # continuation probability (in the same matching)
